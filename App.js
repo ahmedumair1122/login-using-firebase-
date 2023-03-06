4fd052d3f2e412cd0376b17aa1507e94c08caee9
Umair Ahmed <86409567+ahmedumair1122@users.noreply.github.com>
2023-03-06 20:34:27 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+// navigator
+import MainNavigator from './src/nevigation/MainNevigator';
+
+export default function App() {
+  return (
+  <MainNavigator />
+  
+  );
+}
